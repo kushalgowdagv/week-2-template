d9693590a9439f274d39eb504606eaad5b23d46c
kushalgowdagv <104550287+kushalgowdagv@users.noreply.github.com>
2024-09-07 17:47:48 -0400

    " Part 1 of graph creation "

--- student_code.py
+++ student_code.py
@@ -3,7 +3,18 @@
 node_dict={'a':0,'b':1,'c':2,'d':3,'e':4}
 
 def part_1_graph():
-    pass
+
+    # Each index in the list corresponds to the node, and the set contains the nodes it points to using list of sets
+    
+    graph = [
+        {'b', 'e'},  # a points to b and e
+        {'a', 'c'},  # b points to c and also back to a.
+        {'d', 'e'},  # c points to d and e.
+        {'b'},       # d points back to b.
+        set()        # e has no outgoing connections.
+    ]
+    
+    return graph
 
 def part_2_graph():
     pass
