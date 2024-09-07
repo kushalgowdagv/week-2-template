d080b1be0286052af77ab17fef6c732a7202cf55
kushalgowdagv <104550287+kushalgowdagv@users.noreply.github.com>
2024-09-07 17:59:25 -0400

    " part 2 graph "

--- student_code.py
+++ student_code.py
@@ -5,7 +5,7 @@ node_dict={'a':0,'b':1,'c':2,'d':3,'e':4}
 def part_1_graph():
 
     # Each index in the list corresponds to the node, and the set contains the nodes it points to using list of sets
-    
+
     graph = [
         {'b', 'e'},  # a points to b and e
         {'a', 'c'},  # b points to c and also back to a.
@@ -17,8 +17,19 @@ def part_1_graph():
     return graph
 
 def part_2_graph():
-    pass
 
+    # Each index in the list corresponds to the node, and the set contains the nodes it points to using list of lists
+
+
+    graph = [
+        ['b', 'e', 'a'],  # a points to b, e, and itself (a)
+        ['c'],            # b points to c
+        ['a', 'e', 'd'],  # c points to a, e, and d
+        [],               # d doesn't point anywhere
+        ['d']             # e points to d
+    ]
+    
+    return graph
 def part_3_graph():
     pass
 
